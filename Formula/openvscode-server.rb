2deb4830d3b53bafbec8795bef2a323874858aa0
Geoffrey Huntley <ghuntley@ghuntley.com>
2021-09-17 12:55:05 +0000

    [broken]: openvscode-server

---
+++ Formula/openvscode-server.rb
@@ -0,0 +1,33 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class OpenvscodeServer < Formula
+  desc "OpenVSCode Server runs on a remote machine and allows access through a modern web browser."
+  homepage ""
+  url "https://github.com/gitpod-io/openvscode-server/releases/download/openvscode-server-v1.60.0/openvscode-server-v1.60.0-linux-x64.tar.gz"
+  sha256 "88ed7842a60191a307abde16cedc79487fa55166928eba69e9a8e7ca0ab4b9a4"
+  license "MIT"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test openvscode-server`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
